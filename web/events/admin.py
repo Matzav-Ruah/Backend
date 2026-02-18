4ea1a73f2d2fc144dbc7d9ee64e28a3329984b94
TabarakoAkula <mishabor2367@gmail.com>
2026-02-18 22:58:56 +0200

    Create Event admin panel

---
+++ web/events/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Event
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("emotional_state", "created_at", "updated_at")
+    list_filter = ("emotional_state", "created_at", "updated_at")
+    ordering = ("-updated_at",)
