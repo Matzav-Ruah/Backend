b50e751aa966e9d48a9fb2590ed15c5e335ee755
TabarakoAkula <mishabor2367@gmail.com>
2026-02-18 23:40:51 +0200

    Add CUD endpoints for events

--- web/events/api.py
+++ web/events/api.py
@@ -16,3 +16,33 @@ def get_all_events(request):
 @api.get("/get-event", response=schemas.EventSchema)
 def get_event(request, event_id: int):
     return get_object_or_404(Event, id=event_id, user=request.user)
+
+
+@api.post("/create-event", response={201: schemas.EventSchema})
+def create_event(request, payload: schemas.CreateEventSchema):
+    event = Event.objects.create(
+        user=request.user,
+        emotional_state=payload.emotional_state,
+        data=payload.data,
+    )
+    return 201, event
+
+
+@api.put("/update-event/{event_id}", response=schemas.EventSchema)
+def update_event(request, event_id: int, payload: schemas.UpdateEventSchema):
+    event = get_object_or_404(Event, id=event_id, user=request.user)
+
+    if payload.emotional_state is not None:
+        event.emotional_state = payload.emotional_state
+    if payload.data is not None:
+        event.data = payload.data
+
+    event.save()
+    return event
+
+
+@api.delete("/delete-event/{event_id}", response={200: dict})
+def delete_event(request, event_id: int):
+    event = get_object_or_404(Event, id=event_id, user=request.user)
+    event.delete()
+    return 200, {"success": True}
--- web/events/schemas.py
+++ web/events/schemas.py
@@ -1,11 +1,25 @@
+from typing import Literal, Optional
 from pydantic import BaseModel, ConfigDict
 from datetime import datetime
+
+EMOTIONAL_STATE_CHOICES = Literal["bad", "neutral", "good"]
 
 
 class EventSchema(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
+    id: int
     emotional_state: str
     data: dict
     created_at: datetime
     updated_at: datetime
+
+
+class CreateEventSchema(BaseModel):
+    emotional_state: EMOTIONAL_STATE_CHOICES
+    data: dict
+
+
+class UpdateEventSchema(BaseModel):
+    emotional_state: Optional[EMOTIONAL_STATE_CHOICES] = None
+    data: Optional[dict] = None
