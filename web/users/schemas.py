e38056071a7a7570cd989450cc7aaa58e804307d
TabarakoAkula <mishabor2367@gmail.com>
2026-02-18 23:56:09 +0200

    Create User admin panel & Update endpoints messages

--- web/users/schemas.py
+++ web/users/schemas.py
@@ -1,6 +1,20 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+from datetime import datetime
 
 
 class SignInSchema(BaseModel):
     email: str
     password: str
+
+
+class UserSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    settings: dict
+    created_at: datetime
+    updated_at: datetime
